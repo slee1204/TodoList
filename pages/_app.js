66724ad52397d733c32d64a3561fef428be6026c
SueLee <slee1008@my.bcit.ca>
2023-04-01 23:00:30 -0700

    add login session

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-
+import NavMenu from '@/comp/NavMenu'
 import { SessionProvider } from "next-auth/react"
 
 
@@ -8,6 +8,8 @@ export default function App({ Component, pageProps: {session, ...pageProps} }) {
   <>
   {/* //session = {session}인지 pageProps.session인지 확인해야함 */}
     <SessionProvider session={session}>
+      <NavMenu href="/">Home</NavMenu>
+      <NavMenu href="/addNote">To Do Lists</NavMenu>
       <Component {...pageProps} />
     </SessionProvider>
   </>
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,33 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
 import NavMenu from '@/comp/NavMenu'
-// import TodoInput from '@/comp/TodoInput'
 import Link from 'next/link'
 
-const inter = Inter({ subsets: ['latin'] })
+//nextAuth
+import { useSession, signIn, signOut } from 'next-auth/react'
+import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]'
+
 
 export default function Home() {
+  const { data: session } = useSession()
+
+  if (session) {
+    return(
+      <>
+        Signed in as {session.user.email} <br/>
+        <img src= {session.user.image} style={{width:"100px", height:"100px"}}/> <br/>
+        Signed in as {session.user.name} <br/>
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  return (
+    <>
+    Not signed in <br/>
+    <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
   return (
     <>
       <Head>
@@ -19,8 +40,30 @@ export default function Home() {
       <main>
         <NavMenu href="/">Home</NavMenu>
         <NavMenu href="/addNote">To Do Lists</NavMenu>
-        <h1>Todo List</h1>
+        <h1>Make your ToDo List</h1>
+        
+
       </main>
     </>
   )
+}
+
+//get session info from the server side and use it on client side
+export async function getServerSideProps(context) {
+  const session = await getServerSession(context.req, context.res, authOptions)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/api/auth/signin',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {
+      session
+    },
+  }
 }
