88075395681119725f4e4b12105e04e97c6462b0
SueLee <slee1008@my.bcit.ca>
2023-04-02 00:50:04 -0700

    display todo list associated with the user

--- pages/profile.js
+++ pages/profile.js
@@ -2,18 +2,35 @@
 import { useSession, signIn, signOut } from 'next-auth/react'
 import { getServerSession } from "next-auth/next"
 import { authOptions } from './api/auth/[...nextauth]'
+import { useRouter } from 'next/router'
 
-
-export default function Profile() {
+export default function Profile({notesArray}) {
+  const router = useRouter();
   const { data: session } = useSession()
 
   if (session) {
+    const todoNotes = notesArray.map((note) => (
+      <div key={note.id} style={{backgroundColor:"lavender"}}>
+        <div>
+          <p>{note.todo}</p>
+          <p>{note.date}</p>
+          <p>{note.time}</p>
+        </div>
+      </div>
+    ))
     return(
       <>
-        Signed in as {session.user.email} <br/>
-        <img src= {session.user.image} style={{width:"100px", height:"100px"}}/> <br/>
-        Signed in as {session.user.name} <br/>
-        <button onClick={() => signOut()}>Sign out</button>
+        <div>
+          Signed in as {session.user.email} <br/>
+          <img src= {session.user.image} style={{width:"100px", height:"100px"}}/> <br/>
+          Signed in as {session.user.name} <br/>
+          <button onClick={() => router.push("/addNote")}>Post something</button>
+          <button onClick={() => signOut()}>Sign out</button>
+        </div>
+        
+        <div>
+          {todoNotes}
+        </div>
       </>
     )
   }
@@ -28,6 +45,17 @@ export default function Profile() {
 
 
 export async function getServerSideProps(context) {
+
+  const notes = await prisma.note.findMany({
+    orderBy:{
+      createdAt: 'desc'
+    },
+    include: {
+      user: true,
+    }
+  })
+
+
   const session = await getServerSession(context.req, context.res, authOptions)
 
   if (!session) {
@@ -40,9 +68,13 @@ export async function getServerSideProps(context) {
     }
   }
 
+  const notesLists = JSON.parse(JSON.stringify(notes))|| [];
+  const notesArray = Object.values(notesLists);
+  console.log(notesArray)
   return {
     props: {
       session,
+      notesArray,
     },
   }
 }
